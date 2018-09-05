8db998dfea576cb61b008dad8a907db0f999a6ec
Kirpal <skripal77777@gmail.com>
2018-09-05 20:10:28 +0530

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
@@ -7,6 +9,33 @@ import { ProductComponent } from './product/product.component';
 import { ProductAddComponent } from './product-add/product-add.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'products',
+    component: ProductComponent,
+    data: { title: 'Product List' }
+  },
+  {
+    path: 'product-details/:id',
+    component: ProductDetailComponent,
+    data: { title: 'Product Details' }
+  },
+  {
+    path: 'product-add',
+    component: ProductAddComponent,
+    data: { title: 'Product Add' }
+  },
+  {
+    path: 'product-edit/:id',
+    component: ProductEditComponent,
+    data: { title: 'Product Edit' }
+  },
+  { path: '',
+    redirectTo: '/products',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -17,8 +46,11 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
     ProductEditComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RestService } from '../rest.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-product',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-  constructor() { }
+  products:any = [];
+
+  constructor(public rest:RestService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    this.products = [];
+    this.rest.getProducts().subscribe((data: {}) => {
+      console.log(data);
+      this.products = data;
+    });
+  }
+
+  add() {
+    this.router.navigate(['/product-add']);
+  }
+
+  delete(id) {
+    this.rest.deleteProduct(id)
+      .subscribe(res => {
+          this.getProducts();
+        }, (err) => {
+          console.log(err);
+        }
+      );
   }
 
 }
